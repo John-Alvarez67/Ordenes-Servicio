6671ceb3206ef98b1d92970e62e1cfef730ebadf
JuanDiegoCueroRojas <juan.diego.cuero@correounivalle.edu.co>
2024-12-06 00:59:27 -0500

    glsd

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask
 from flask_login import LoginManager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from usuario import Base, Usuario
 import os
+from routes import routes  # Importa el Blueprint
 
 app = Flask(__name__)
 app.secret_key = 'clave_secreta'
-
-# Configuración para archivos estáticos
-app.static_folder = 'static'
-app.static_url_path = '/static'
 
 # Configuración de Login Manager
 login_manager = LoginManager()
@@ -18,28 +15,28 @@ login_manager.init_app(app)
 login_manager.login_view = 'inicio_sesion'
 
 # Cadena de conexión a PostgreSQL desde variables de entorno
-connection_string = os.getenv('DATABASE_URL')  # Asegúrate de que esta variable esté configurada en Render
+connection_string = os.getenv('DATABASE_URL')
 
 # Motor y sesión de base de datos
 engine = create_engine(connection_string)
 Base.metadata.create_all(engine)
-Session = scoped_session(sessionmaker(bind=engine))  # Usar scoped_session para manejo más seguro
+Session = scoped_session(sessionmaker(bind=engine))
 
 # Cargar usuario para sesión
 @login_manager.user_loader
 def load_user(user_id):
     session = Session()
     try:
-        return session.get(Usuario, user_id)  # Si estás usando SQLAlchemy 1.4+
+        return session.get(Usuario, user_id)
     finally:
         session.close()
 
-# Rutas importadas desde otro módulo
+# Registrar el Blueprint
+app.register_blueprint(routes)
+
 if __name__ == '__main__':
-    from routes import *
     app.run(debug=True)
 
-# Asegurarse de remover la sesión al final del ciclo de vida de la app
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     Session.remove()
